68590d237499a54990828cfeb86f03628d1bab3c
Nafees Haider <haidernafees0907@gmail.com>
2025-09-27 14:59:34 +0000

    https://forexsignal-iota.vercel.app/login check its still not working M]

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -53,7 +53,7 @@ export default function LoginPage() {
                   <FormItem>
                     <FormLabel>Email</FormLabel>
                     <FormControl>
-                      <Input placeholder="name@example.com" {...field} />
+                      <Input placeholder="name@example.com" {...field} autoComplete="email" />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -66,7 +66,7 @@ export default function LoginPage() {
                   <FormItem>
                     <FormLabel>Password</FormLabel>
                     <FormControl>
-                      <Input type="password" placeholder="********" {...field} />
+                      <Input type="password" placeholder="********" {...field} autoComplete="current-password"/>
                     </FormControl>
                     <FormMessage />
                   </FormItem>
